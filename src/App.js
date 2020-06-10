7a1f47dd9adf6c19a049b38f0dc81cbcf6659e61
Farhan Ahmed <Farhan.ahmed5160@gmail.com>
2020-06-10 23:22:06 +0500

    Bootcamp 3rd Assignment Done!

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Dinner from './test';
+
+function App() {
+  return (
+    <div className="App App-header">
+
+        <Dinner dish="Chicken Biryani"/>
+        <Dinner dish="Chicken Karahi"/>
+        <Dinner dish="Nihaari"/>
+        
+    </div>
+  );
+}
+
+export default App;
